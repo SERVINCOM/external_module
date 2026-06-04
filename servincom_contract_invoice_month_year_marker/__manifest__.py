ed703e6cbd1dea9378648df2f13b9e9662c98db4
SERVINCOM SOLUCIONES <afebles@servincom.es>
2026-06-04 22:46:11 +0100

    Add line break between invoice marker legend entries

--- servincom_contract_invoice_month_year_marker/__manifest__.py
+++ servincom_contract_invoice_month_year_marker/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "SERVINCOM Contract Invoice Month Year Marker",
-    "version": "18.0.1.0.7",
+    "version": "18.0.1.0.8",
     "summary": "Adds invoice month and year markers to contracts",
     "author": "SERVINCOM SOLUCIONES, S.L.",
     "license": "AGPL-3",
