6a3d093acff2346fb29d6ffa269caf8aac18d33d
SERVINCOM SOLUCIONES <afebles@servincom.es>
2026-06-25 12:43:41 +0100

    [16.0] Load open cash drawer POS assets

--- servincom_pos_open_cash_drawer/__manifest__.py
+++ servincom_pos_open_cash_drawer/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "SERVINCOM POS Open Cash Drawer",
-    "version": "16.0.1.4.0",
+    "version": "16.0.1.5.0",
     "summary": "Open the configured POS cash drawer from the product screen",
     "category": "Point of Sale",
     "author": "SERVINCOM SOLUCIONES, S.L.",
@@ -11,6 +11,13 @@
     "depends": [
         "point_of_sale",
     ],
+    "assets": {
+        "point_of_sale.assets": [
+            "servincom_pos_open_cash_drawer/static/src/css/open_cash_drawer_button.css",
+            "servincom_pos_open_cash_drawer/static/src/js/open_cash_drawer_button.js",
+            "servincom_pos_open_cash_drawer/static/src/xml/open_cash_drawer_button.xml",
+        ],
+    },
     "installable": True,
     "application": False,
 }
